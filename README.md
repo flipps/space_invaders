66751385a4716f705938bd8298b983ff5625f7de
Filipe Morais <fi.morais.mail@gmail.com>
2018-09-28 18:26:07 -0300

    first commit, just finished collision checking.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Space Invaders
+
+## This is an attempt to recreate space invaders with lua language and Love2d framework.
+
+to run it: 
+
+- Check if you have lua installed on your machine, if not, get it at: https://www.lua.org/download.html
+- Download/install love2d: https://love2d.org/
+- To run the game from the terminal you can define the path to love2d temporary if you’re on a Mac or Linux (more instructions on Windows can be found at - https://love2d.org/wiki/PATH): 
+   `export PATH=$PATH:/Applications/love.app/Contents/MacOS/`
+- And then from the `main.lua` folder run the command `love .`
+
+
---
+++ main.lua
@@ -0,0 +1,130 @@
+enemy = {}
+enemies_controller = {}
+enemies_controller.enemies = {}
+enemy_space_between = 20
+
+function love.load()
+    player = {}
+    player.width = 80
+    player.height = 20
+    player.bullets = {}
+    player.speed = 5
+    player.fireCooldown = 20
+    player.x = 0
+    player.y = 550
+    player.fire = function()
+        if player.fireCooldown <= 0 then
+            player.fireCooldown = 20
+            bullet = {}
+            bullet.size = 5
+            bullet.x = player.x + ((player.width / 2) - (bullet.size / 2))
+            bullet.y = player.y
+            bullet.speed = 10
+            table.insert(player.bullets, bullet)
+        end
+    end
+    
+    for i=3, 1, -1 do
+        enemies_controller:spawnEnemy(love.math.random(20, 780), 0)
+    end
+
+end
+
+function enemies_controller:spawnEnemy(x, y)
+    enemy = {}
+    enemy.x = x + enemy_space_between
+    enemy.y = y
+    enemy.size = 20
+    enemy.speed = 2
+    enemy.cooldown = 20
+    table.insert(self.enemies, enemy)
+end
+
+function enemy:fire() -- enemy:fire is short for enemy.fire(self) which is a reference to the table we created at the load function.
+    if self.cooldown <= 0 then
+        self.cooldown = 20
+        bullet = {}
+        bullet.x = self.x - (self.size / 2)
+        bullet.y = self.y
+        table.insert(enemies, bullet)
+    end
+end
+
+function movePlayer()
+    if love.keyboard.isDown('right') then
+        player.x = player.x + player.speed
+    elseif love.keyboard.isDown('left') then
+        player.x = player.x - player.speed
+    end
+    
+    --Lock player to the screen width (Love game window size default is 800x600)
+    if player.x >= (800 - player.width) then
+        player.x = 800 - player.width
+    elseif player.x < 0 then
+        player.x = 0
+    end
+end
+
+function checkBulletEnemyCollision(ax, ay, bx, by, ar, br)
+	local dx = bx - ax
+	local dy = by - ay
+    return dx * dx + dy * dy < (ar + br) * (ar + br)
+end
+
+function love.update(dt)
+
+    player.fireCooldown = player.fireCooldown - 1
+
+    movePlayer()
+
+    -- Fire!
+    if love.keyboard.isDown('space') then
+        player.fire()
+    end
+    
+    -- If the bullet moves out of the screen remove it from the table(performance issue).
+    for i,b in ipairs(player.bullets) do
+        if b.y < -5 then
+            table.remove(player.bullets, i)
+        end
+
+        b.y = b.y - b.speed
+    end
+
+    -- Enemies positioning
+    for i,e in ipairs(enemies_controller.enemies) do
+        if e.y >= 620 then
+            table.remove(enemies_controller.enemies, i)
+        end
+
+        e.y = e.y + e.speed
+    end
+
+    -- Check enemies and bullets collision
+    for i,b in ipairs(player.bullets) do
+        for j,e in ipairs(enemies_controller.enemies) do
+            if checkBulletEnemyCollision(b.x, b.y, e.x, e.y, b.size, e.size) then
+                table.remove(player.bullets, i)
+                table.remove(enemies_controller.enemies, j)
+            end
+        end
+    end
+end
+
+function love.draw()
+    -- Draw player
+    love.graphics.setColor(255, 255, 0)
+    love.graphics.rectangle('fill', player.x, player.y, player.width, player.height)
+    
+    -- Draw enemies
+    for _,e in pairs(enemies_controller.enemies) do
+        love.graphics.setColor(255, 0, 0)
+        love.graphics.rectangle('fill', e.x, e.y, e.size, e.size)
+    end
+
+    -- Draw bullets
+    for _,b in pairs(player.bullets) do
+        love.graphics.setColor(255, 255, 255)
+        love.graphics.rectangle('fill', b.x, b.y, b.size, b.size)
+    end
+end
